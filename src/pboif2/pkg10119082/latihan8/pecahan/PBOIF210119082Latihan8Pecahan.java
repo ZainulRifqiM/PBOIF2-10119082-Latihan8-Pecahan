4bdee50a76b28a7d5bdbebb6cf886622524123e9
Zainul Rifqi <Zainul Rifqi@LAPTOP-O1E2M14H>
2020-10-17 16:58:01 +0700

    PBOIF2-10119082-Latihan8-Pecahan

---
+++ src/pboif2/pkg10119082/latihan8/pecahan/PBOIF210119082Latihan8Pecahan.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119082.latihan8.pecahan;
+
+/**
+ *
+ * @author Zainul Rifqi
+ * NAMA     : Zainul Rifqi Muwaffaq
+ * KELAS    : IF2
+ * NIM      : 10119082
+ * Deskripsi Program : Pecahan
+ */
+public class PBOIF210119082Latihan8Pecahan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        float f;
+        double d;
+        f = 1234567890.123456789F;
+        d = 1_234_567_890.1234567890;
+        System.out.println("Float : " + f);
+        System.out.println("Double : " + d);
+    }
+    
+}
